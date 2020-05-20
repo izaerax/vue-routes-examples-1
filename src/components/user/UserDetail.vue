542e763906be02dcd7a5328c3236d3382a5ed549
olti <dajce.info@gmail.com>
2020-05-20 11:52:53 +0200

    query params

--- src/components/user/UserDetail.vue
+++ src/components/user/UserDetail.vue
@@ -2,6 +2,9 @@
     <div>
         <h3>Some User Details</h3>
         <p>User id: {{ $route.params.id }}</p>
-        <router-link tag="button" class="btn btn-primary" :to="{ name: 'userEdit', params: {id: $route.params.id} }">Edit</router-link>
+        <router-link
+                tag="button" class="btn btn-primary"
+                :to="{ name: 'userEdit', params: {id: $route.params.id}, query: { locale: 'it', q: 100} }"
+        >Edit</router-link>
     </div>
 </template>
