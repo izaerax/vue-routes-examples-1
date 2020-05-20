7a290b04491c1ff1c58ec0d3e5fae9f40ffdb247
olti <dajce.info@gmail.com>
2020-05-20 11:46:52 +0200

    nested paths and naming

--- src/components/user/UserDetail.vue
+++ src/components/user/UserDetail.vue
@@ -1,3 +1,7 @@
 <template>
-    <h3>Some User Details</h3>
+    <div>
+        <h3>Some User Details</h3>
+        <p>User id: {{ $route.params.id }}</p>
+        <router-link tag="button" class="btn btn-primary" :to="{ name: 'userEdit', params: {id: $route.params.id} }">Edit</router-link>
+    </div>
 </template>
