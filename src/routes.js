6b4d96ec038f40478357b6cd613634ee56297b61
olti <dajce.info@gmail.com>
2020-05-20 12:05:56 +0200

    redirect

--- src/routes.js
+++ src/routes.js
@@ -22,5 +22,6 @@ export const routes = [
             {path: ':id', component: UserDetail},
             {path: ':id/edit', component: UserEdit, name: 'userEdit'}
         ]
-    }
+    },
+    { path: '/*', redirect: { name: 'home'}}
 ];
