b3d496339a59597487a2f4e3de204f01e5e61f93
olti <dajce.info@gmail.com>
2020-05-20 12:02:08 +0200

    naming router views

--- src/routes.js
+++ src/routes.js
@@ -3,10 +3,21 @@ import Home from './components/Home.vue'
 import UserStart from './components/user/UserStart.vue'
 import UserDetail from './components/user/UserDetail.vue'
 import UserEdit from './components/user/UserEdit.vue'
+import Header from "./components/Header.vue";
 
 export const routes = [
-    { path: '', component: Home, name: 'home'},
-    { path: '/user', component: User, children: [
+    { path: '', name: 'home',
+        components: {
+            default: Home,
+            'header-top': Header
+        }
+    },
+    { path: '/user', component: User,
+        components: {
+            default: User,
+            'header-bottom': Header
+        },
+        children: [
             {path: '', component: UserStart},
             {path: ':id', component: UserDetail},
             {path: ':id/edit', component: UserEdit, name: 'userEdit'}
